a6ff3e4d924f6323f1c4874240215729bc1c1adb
PARK MIYEON <miyoun1003@naver.com>
2023-06-23 15:41:54 +0900

    productlist 만드는 중

--- src/main/java/kr/ac/kopo/product/ProductListController.java
+++ src/main/java/kr/ac/kopo/product/ProductListController.java
@@ -1,5 +1,23 @@
 package kr.ac.kopo.product;
 
-public class ProductListController {
+import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.ac.kopo.biz.product.ProductDAO;
+import kr.ac.kopo.biz.product.ProductVO;
+import kr.ac.kopo.controller.Controller;
+
+public class ProductListController implements Controller{
+
+	@Override
+	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+		ProductDAO dao = new ProductDAO();
+		List<ProductVO> product = dao.
+		return "";
+	}
+
+	
+	
 }
